c88d1a990eb65e6bc66f5ceaea091f4c2da76ca3
sjy8336 <sjy8336@gmail.com>
2024-09-05 11:19:45 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
-const webS = new Swiper('.web_slide', {
-    direction: 'vertical',
+const webS = new Swiper('.web_s', {
     loop: true,
-    autoplay:{delay:1500},
+    autoplay:{delay:2000},
+    navigation:{
+        prevEl:'.project01 .pj01_in .swiper-button-prev',
+        nextEl:'.project01 .pj01_in .swiper-button-next',
+    },
 });
