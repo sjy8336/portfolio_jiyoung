3f1a4070db85bee5df5d6978064f5b15a02da85f
sjy8336 <sjy8336@gmail.com>
2024-09-25 12:44:19 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,15 +1,11 @@
 const webS = new Swiper('.web_s', {
     loop: true,
     autoplay:{delay:2000},
-    navigation:{
-        prevEl:'.project01 .pj01_in .swiper-button-prev',
-        nextEl:'.project01 .pj01_in .swiper-button-next',
-    },
 });
-    const ports = new Swiper(".main",{
-        direction:'vertical',
-        mousewheel:true,
-        speed:1000,
-    })
-    const swiperEl = document.querySelector('swiper-container');
-    const swiper = swiperEl.swiper;
+const ports = new Swiper(".main",{
+    direction:'vertical',
+    mousewheel:true,
+    speed:1000,
+})
+const swiperEl = document.querySelector('swiper-container');
+const swiper = swiperEl.swiper;
